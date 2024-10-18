5884b325804d9a8e0e6bd77810954f626a5908bc
Matheus Sousa Chimendes <matheus.chimendes@alloha.com>
2024-10-18 16:01:54 -0300

    atualziando os texto do projeto

--- script/script.js
+++ script/script.js
@@ -1,63 +1,63 @@
 
 const textos = {
-    falhaEletrica: `# Falha elétrica #
-
-Prezados, boa noite.
+    falhaEletrica: `
+Prezados,
 
 Verificamos que o equipamento está com alarme de falha elétrica, poderia verificar e validar essa informação por gentileza?
 
-Segue protocolo do chamado `,
+Segue protocolo do chamado: {{PROTOCOLO}}`,
 
-    equipamentoOk: `# Equipamento ok #
+    equipamentoOk: `
 
-Prezados, boa noite!
+Prezados, 
 
 Verificamos que o equipamento se encontra operacional, poderia validar por gentileza?
 
-Segue protocolo do chamado `,
+Segue protocolo do chamado: {{PROTOCOLO}}`,
 
-    enviadoCampo: `# Enviado para CAMPO #
+    enviadoCampo: `
 
-Prezados, boa tarde.
+Prezados, 
 
-Seguimos a abertura de seu chamado no protocolo:
+Seguimos a abertura de seu chamado no protocolo: {{PROTOCOLO}} 
 
 Informamos que foi encontrada uma falha no equipamento, técnico em campo foi acionado e enviado até o local realizando reconfiguração do equipamento e restabelecendo a conexão.
 
 Qualquer dúvida estamos à disposição.`,
 
-informacoesInsuficientes: `# Informações insuficientes. #
+    informacoesInsuficientes: `
 
-Prezados, boa tarde.
+Prezados, 
 
-Seguimos a abertura do seu chamado no protocolo: 
+Seguimos a abertura do seu chamado no protocolo:  {{PROTOCOLO}} 
 
-Porem identificamos que não existem informações o suficiente para seguir a análise de seu chamado. Pedimos que os senhores nos respondam com as seguintes informações, por gentileza:
+Porem identificamos que não existem informações o suficiente para seguir a análise de seu chamado. 
+Pedimos que os senhores nos respondam com as seguintes informações, por gentileza.
 `,
 
-    cancelamentoChamado: `# Chamado Cancelado #
+    cancelamentoChamado: `
 
-Prezados, bom dia!
+Prezados,
 
 Informo que essa solicitação está sendo tratada no chamado #24092024-32771 - INC0225576 - [High] LINK-DEDICADO-WIRELINK-CE-ESTACIO-IGUATU-186.225.38.219. 
 
-Por isso, o chamado que foi recentemente aberto 26092024-18714 será cancelado.
+Por isso, o chamado que foi recentemente aberto {{PROTOCOLO}} será cancelado.
 `,
 
-    emailProtocolo: `# E-mail com protocolo #
+    emailProtocolo: `
 
-Prezados, boa tarde!
+Prezados,
 
-Informo que foi gerado o protocolo: 17102024-76174, e que seu caso foi enviado para a equipe de analistas. Logo mais retornaremos com atualizações sobre o seu caso.
+Informo que foi gerado o protocolo:  {{PROTOCOLO}}, e que seu caso foi enviado para a equipe de analistas. Logo mais retornaremos com atualizações sobre o seu caso.
 `,
 
-    falhaEletricaOperacional: `# Falha elétrica mas operacional #
+    falhaEletricaOperacional: `
 
-Prezados, boa tarde.
+Prezados, 
 
 Verificamos que houve uma falha elétrica no local, porém o equipamento já se encontra operacional, poderia validar por gentileza?
 
-Segue protocolo do chamado 26092024-82853.
+Segue protocolo do chamado: {{PROTOCOLO}}
 `
 };
 
@@ -78,7 +78,7 @@ selectTexto.addEventListener('change', () => {
     const tipoTexto = selectTexto.value;   //Pega o valor selecionado no dropdown
 
     if (tipoTexto && textos[tipoTexto]) {
-        textareaTextoGerado.value = textos[tipoTexto] + protocolo;
+        textareaTextoGerado.value = textos[tipoTexto].replace('{{PROTOCOLO}}',  protocolo);
     } else {
         textareaTextoGerado.value = '';
     }
